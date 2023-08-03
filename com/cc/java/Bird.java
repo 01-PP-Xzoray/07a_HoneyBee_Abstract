12a2e5d75826243c02ed53aacc45f98ee52642db
Sohrab <sohrab-arenja8@hotmail.de>
2023-08-03 10:33:14 +0200

    Bird

---
+++ com/cc/java/Bird.java
@@ -0,0 +1,16 @@
+package com.cc.java;
+
+public class Bird extends HoneyBee{
+
+    @Override
+    public String doYourJob() {
+        return "But I am a Bird, gurr gurr!";
+    }
+
+    @Override
+    public String fly() {
+        return "I'm a Bird and I like to vögeln!";
+    }
+    
+    
+}
